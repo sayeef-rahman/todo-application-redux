10cdcd965295fdbc4c6192db9cd7eb171aca6872
sayeef-rahman <sayeef.cse@gmial.com>
2022-09-27 13:18:04 +0600

    complete

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from "react-redux";
+import { colorChanged, statusChanged } from "../redux/filters/actions";
+
+const numberOfTodos = (number_of_todos) =>{
+    switch (number_of_todos) {
+        case 0:
+            return "No Task";
+        
+        case 1:
+            return "1 Task"
+    
+        default:
+            return `${number_of_todos} Tasks`
+    }
+}
+
+export default function Footer() {
+    const todos = useSelector((state)=>state.todos);
+    const filters = useSelector((state) => state.filters);
+    const {status, colors} = filters;
+    const dispatch = useDispatch();
+    const todosRemaining = todos.filter((todo) => !todo.completed).length;
+
+    const handleStatus = (status) =>{
+        dispatch(statusChanged(status));
+    }
+    const handleColorChnaged = (color) =>{
+        if(colors.includes(color)){
+            dispatch(colorChanged(color, "removed"));
+        }else{
+            dispatch(colorChanged(color, "added"));
+        }
+    }
+    return (
+        <div className="mt-4 flex justify-between text-xs text-gray-500">
+            <p>{numberOfTodos(todosRemaining)} left</p>
+            <ul className="flex space-x-1 items-center text-xs">
+                <li className={`cursor-pointer ${status === "All" && "font-bold"}`} onClick={()=>{handleStatus("All")}}>All</li>
+                <li>|</li>
+                <li className={`cursor-pointer ${status === "Incomplete" && "font-bold"} `} onClick={()=>{handleStatus("Incomplete")}}>Incomplete</li>
+                <li>|</li>
+                <li className={`cursor-pointer ${status === "Complete" && "font-bold"} `} onClick={()=>{handleStatus("Complete")}}>Complete</li>
+                <li></li>
+                <li></li>
+                <li className={`h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer ${colors.includes("green") && 'bg-green-500'}`} onClick={()=>handleColorChnaged("green",)}></li>
+                <li className={`h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer ${colors.includes("red") && 'bg-red-500'}`} onClick={()=>handleColorChnaged("red",)}></li>
+                <li className={`h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer ${colors.includes("yellow") && 'bg-yellow-500'}`} onClick={()=>handleColorChnaged("yellow",)}></li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,39 @@
+import { useSelector } from "react-redux";
+import Todo from "./Todo";
+
+export default function TodoList() {
+    const todos = useSelector((state) => state.todos);
+    const filters = useSelector((state) => state.filters);
+    const filterByStatus = (todo) => {
+        const {status} = filters;
+        switch (status) {
+            case "Complete":
+                return todo.completed;
+            
+            case "Inomplete":
+                return !todo.completed;
+        
+            default:
+                return true;
+            }
+    };
+
+    const filterByColors = (todo) =>{
+        const {colors} = filters;
+        if(colors.length > 0){
+            return colors.includes(todo?.color);
+        }
+        else{
+            return true; 
+        }
+    };
+
+    return (
+        <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+           { todos
+           .filter((todo)=> filterByStatus(todo))
+           .filter((todo) => filterByColors(todo))
+           .map(todo => <Todo todo={todo} key={todo.id} /> )}
+        </div>
+    );
+}
